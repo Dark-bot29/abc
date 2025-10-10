3c8f083e93a52375228aff9c0188c8af21070d6d
Dark-bot29 <darkbot490@gmail.com>
2025-10-10 09:30:40 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Your SESSION ID Put' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=gNgHyLhT#MJ9G9zEoqQqmB3V7I0G99Sj7ySRMrljISf44y_2-XJc
+    ' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
